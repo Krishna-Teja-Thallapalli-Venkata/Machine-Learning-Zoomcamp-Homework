a745c5c3fb03a475f8138f31530fa47b85bbdeec
Krishna-Teja-Thallapalli-Venkata <153772019+Krishna-Teja-Thallapalli-Venkata@users.noreply.github.com>
2025-10-25 15:11:38 +0000

    Test

--- 05-Deployment/Predict.py
+++ 05-Deployment/Predict.py
@@ -1,37 +1,36 @@
 import pickle
-
+from flask import Flask
+from flask import request
+from flask import jsonify
+ 
 model_file = 'model_C=1.0.bin'
-
+ 
 with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
  
-dv, model
-# Output: (DictVectorizer(sparse=False), LogisticRegression(max_iter=1000))
-
-customer = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
-
-X = dv.transform([customer])
-X
-
-model.predict_proba(X)
-model.predict_proba(X)[0,1]
+app = Flask('churn')
+ 
+@app.route('/predict', methods=['POST'])
+def predict():
+    # json = Python dictionary
+    customer = request.get_json()
+ 
+    X = dv.transform([customer])
+    model.predict_proba(X)
+    y_pred = model.predict_proba(X)[0,1] 
+    churn = y_pred >= 0.5
+ 
+    result = {
+        # the next line raises an error so we need to change it
+        #'churn_probability': y_pred,
+        'churn_probability': float(y_pred),
+        # the next line raises an error so we need to change it
+        #'churn': churn
+        'churn': bool(churn)
+    }
+ 
+    return jsonify(result) 
+ 
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
+print("Program Executed...........")
--- 05-Deployment/Train.py
+++ 05-Deployment/Train.py
@@ -1,21 +1,19 @@
 import pickle
-
 import pandas as pd
 import numpy as np
- 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
- 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
-
-#parameters
+ 
+# Setting model parameters
 C = 1.0
 n_splits = 5
-
-# Data preparation
  
+output_file = f'model_C={C}.bin'
+ 
+# Data preparation
 df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
  
 df.columns = df.columns.str.lower().str.replace(' ', '_')
@@ -29,11 +27,10 @@ df.totalcharges = pd.to_numeric(df.totalcharges, errors='coerce')
 df.totalcharges = df.totalcharges.fillna(0)
  
 df.churn = (df.churn == 'yes').astype(int)
-
-# Data splitting
  
+# Data splitting
 df_full_train, df_test = train_test_split(df, test_size=0.2, random_state=1)
-
+ 
 numerical = ['tenure', 'monthlycharges', 'totalcharges']
  
 categorical = ['gender', 'seniorcitizen', 'partner', 'dependents',
@@ -41,8 +38,8 @@ categorical = ['gender', 'seniorcitizen', 'partner', 'dependents',
        'onlinesecurity', 'onlinebackup', 'deviceprotection', 'techsupport',
        'streamingtv', 'streamingmovies', 'contract', 'paperlessbilling',
        'paymentmethod']
-
-
+ 
+# Training
 def train(df_train, y_train, C=1.0):
     dicts = df_train[categorical + numerical].to_dict(orient='records')
  
@@ -53,20 +50,23 @@ def train(df_train, y_train, C=1.0):
     model.fit(X_train, y_train)
  
     return dv, model
-
+ 
 def predict(df, dv, model):
      dicts = df[categorical + numerical].to_dict(orient='records')
  
-     X = dv.transform(dicts)
+     X = dv.fit_transform(dicts)
      y_pred = model.predict_proba(X)[:,1]
  
      return y_pred
-
-
-
-kfold = KFold(n_splits=n_splits, shuffle=True, random_state=1)  
  
+# Validation
+print(f'doing validation with C={C}')
+ 
+kfold = KFold(n_splits=n_splits, shuffle=True, random_state=1)  
+     
 scores = []
+ 
+fold = 0
  
 for train_idx, val_idx in kfold.split(df_full_train):
     df_train = df_full_train.iloc[train_idx]
@@ -81,21 +81,28 @@ for train_idx, val_idx in kfold.split(df_full_train):
     auc = roc_auc_score(y_val, y_pred)
     scores.append(auc)
  
-print('C=%s %.3f +- %.3f' % (C, np.mean(scores), np.std(scores)))
+    print(f'auc on fold {fold} is {auc}')
+    fold += 1
  
+print('validation result:')
+print('C=%s %.3f +- %.3f' % (C, np.mean(scores), np.std(scores)))
 # Output: C=1.0 0.841 +- 0.008
-
+ 
+# Train the final model
+print('train the final model')
+ 
 dv, model = train(df_full_train, df_full_train.churn.values, C=1.0)
 y_pred = predict(df_test, dv, model)
 y_test = df_test.churn.values
  
 auc = roc_auc_score(y_test, y_pred)
-auc
-
-output_file = f'model_C={C}.bin'
-# Output: 'model_C=1.0.bin'
-
+ 
+print(f'auc={auc}')
+# Output: 0.8572386167896259
+ 
+# Saving the model with Pickle
 with open(output_file, 'wb') as f_out:
     pickle.dump((dv, model), f_out)
-
-print("Program Executed.....")
+ 
+print(f'the model is saved to {output_file}')
+print("Program Executed..............")
